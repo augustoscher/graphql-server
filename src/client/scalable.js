82346ebf0ff91ad1a00de56c9e2abebfce7af3c4
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-04 16:14:05 -0300

    feat: add scalable client

---
+++ src/client/scalable.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+
+const DEFAULT_TIMEOUT = 1000 * 10 // Wait for 10s
+const POSTS_URL = 'https://www.scalablepath.com/api/test/test-posts'
+const AUTHOR_URL = 'https://www.scalablepath.com/api/test/test-users'
+
+const fetchPosts = () => axios.get(POSTS_URL, { timeout: DEFAULT_TIMEOUT })
+const fetchAuthors = () => axios.get(AUTHOR_URL, { timeout: DEFAULT_TIMEOUT })
+
+const getPosts = async (params = {}) => {
+  const { queryTerm } = params
+  const postsResponse = await fetchPosts()
+  const authorsResponse = await fetchAuthors()
+
+  const posts = postsResponse.data
+  const authors = authorsResponse.data
+
+  const mapped = posts.map((post) => {
+    const { name } = authors.find((author) => author.id === post.userId)
+
+    return {
+      id: post.id,
+      author: name,
+      title: post.title,
+      body: post.body
+    }
+  })
+
+  if (!queryTerm) return mapped
+
+  console.log('filtering')
+  return mapped.filter((post) => {
+    return (
+      post.author.includes(queryTerm) ||
+      post.title.includes(queryTerm) ||
+      post.body.includes(queryTerm)
+    )
+  })
+}
+
+module.exports = getPosts
