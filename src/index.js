52cec0412d3d549431e2f2fc789c74b04ab94f4a
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-11 21:29:44 -0300

    feat: remove trailing comma

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server')
 
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
 
-require('./config/database');
+require('./config/database')
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server
   .listen()
   .then(({ url }) => console.log(`Server ready at ${url}`))
-  .catch((error) => console.log('Server failed: ', error));
+  .catch((error) => console.log('Server failed: ', error))
