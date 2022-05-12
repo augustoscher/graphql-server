a4d5eecead90a731310b3e7bb20871817506d8b9
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-11 21:29:48 -0300

    feat: remove trailing comma

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,24 @@
-const mongoose = require('mongoose');
-const { MONGODB_URL, MONGO_USER, MONGO_PASSWORD, NODE_ENV } = require('./constants');
+const mongoose = require('mongoose')
+const {
+  MONGODB_URL,
+  MONGO_USER,
+  MONGO_PASSWORD,
+  NODE_ENV
+} = require('./constants')
 
 const defaultConfig = {
   user: MONGO_USER,
   pass: MONGO_PASSWORD,
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+  useUnifiedTopology: true
+}
 
 const devConfig = {
   ...defaultConfig,
-  authSource: 'admin',
-};
+  authSource: 'admin'
+}
 
 module.exports = mongoose
   .connect(MONGODB_URL, NODE_ENV === 'development' ? devConfig : defaultConfig)
   .then(() => console.log('Database connected'))
-  .catch((error) => console.log('Databased failed: ', error));
+  .catch((error) => console.log('Databased failed: ', error))
