e7e551a3c010e3841ffbf3f34e50aba2af3b44a1
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-11 21:29:38 -0300

    feat: remove trailing comma

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,12 +2,12 @@ const {
   NODE_ENV = 'development',
   MONGODB_URL = 'mongodb://db:27017/graphql',
   MONGO_USER = 'graphql',
-  MONGO_PASSWORD = 'graphql',
-} = process.env;
+  MONGO_PASSWORD = 'graphql'
+} = process.env
 
 module.exports = {
   NODE_ENV,
   MONGODB_URL,
   MONGO_USER,
-  MONGO_PASSWORD,
-};
+  MONGO_PASSWORD
+}
